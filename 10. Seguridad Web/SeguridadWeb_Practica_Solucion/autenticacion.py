7c6631ffdbcb986ba40ba27e17dffaf7dfd89308
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2018-12-04 12:08:36 +0100

    Cambios Practica 10

--- 10. Seguridad Web/SeguridadWeb_Practica_Solucion/autenticacion.py
+++ 10. Seguridad Web/SeguridadWeb_Practica_Solucion/autenticacion.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 
-#
-# CABECERA AQUI
-#
+
+# Asignatura: Gestión de la Información Web
+# Práctica: Consultas MongoDB
+# Grupo: 10
+# Autores: Sergio Martín, Víctor Chamizo, Fernando Lopez, Pablo Garcia e Irene Martín declaramos
+# que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos sido ayudados
+# por ninguna otra persona ni hemos obtenido la solución de fuentes externas, y tampoco hemos compartido
+# nuestra solución con nadie. Declaramos ademáss que no hemos realizado de manera deshonesta ninguna otra
+# actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los demás.
 
 
-from bottle import run, post
-# Resto de importaciones
+# Imports
+from bottle import get, post, run, template, request, TEMPLATE_PATH
+from pymongo import MongoClient
+from pprint import pprint 
 
+# Templates path
+TEMPLATE_PATH.insert(0, "views")
+
+# Inicializacion de BD
+client = MongoClient('localhost', 27017)
+db = client.giw_prac10
+users = db.users
 
 ##############
 # APARTADO 1 #
@@ -21,8 +36,8 @@ from bottle import run, post
 
 @post('/signup')
 def signup():
-    pass
-    
+    form = request.forms.get("password")
+    print(form)
 
 @post('/change_password')
 def change_password():
